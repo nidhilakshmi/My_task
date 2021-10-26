06b734a67531a1f4bacce2baae281cfaaa74f1cd
srinidhi <srinidhigs1997@gmail.com>
2021-10-26 13:41:42 +0530

    generated controller and model

---
+++ db/migrate/20211026080928_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.string :user_name
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
