89a6c1924a5e8280573423c4696a2848faaa923c
srinidhi <srinidhigs1997@gmail.com>
2021-10-26 21:49:46 +0530

    created crud

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 class TodosController < ApplicationController
+	before_action :find_todo, only: %i[show edit update destroy]
 	def index
+		 @todos = Todo.all
 	end
 
 	def new
@@ -7,17 +9,34 @@ class TodosController < ApplicationController
 	end
 
 	def create
-	@todo = Todo.new todo_params
+	@todo = Todo.new(todo_params)
 
 	if @todo.save
-		redirect_to @post, notice: "Heyy! Todo was created"
+		redirect_to edit_todo_path(@todo), notice: "Heyy! Todo was created"
 	else render new notice: "sorry try next time "
 	end
 	end
+
+	 def show
+	 end
+
+	 def edit
+	 end
+
+	 def update
+	 	if @todo.update.todo_params
+	 		redirect_to @todo, notice: "Heyy! Todo was updated"
+	 	else render new notice: "Sorry we can't update"
+	 	end
+	 end
 
 
 	private 
 	 def todo_params
 	 	params.require(:todo).permit(:user_name,:password)
 	 end
-end
+
+	 def find_todo
+	 	@todo = Todo.find(params[:id])
+	 end
+end 
