b3ca83cfc3c470c43458e7fde68dda02e325fd3e
srinidhi <srinidhigs1997@gmail.com>
2021-10-26 16:38:58 +0530

    new action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,23 @@
 class TodosController < ApplicationController
+	def index
+	end
+
+	def new
+	 @todo = Todo.new
+	end
+
+	def create
+	@todo = Todo.new todo_params
+
+	if @todo.save
+		redirect_to @post, notice: "Heyy! Todo was created"
+	else render new notice: "sorry try next time "
+	end
+	end
+
+
+	private 
+	 def todo_params
+	 	params.require(:todo).permit(:user_name,:password)
+	 end
 end
